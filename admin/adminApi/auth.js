914ed9afdf0efb80499ccf69a98895338d242c1d
ankitchauhan1221 <ankitchauhan1221@gmail.com>
2026-04-22 11:49:26 +0530

    admilogin

---
+++ admin/adminApi/auth.js
@@ -0,0 +1,14 @@
+// adminApi/auth.js
+
+(function () {
+    const isLoggedIn = localStorage.getItem("authToken");
+
+    const currentPage = window.location.pathname.split("/").pop();
+
+    // login page par ho to check na karo
+    if (currentPage === "login.php") return;
+
+    if (!isLoggedIn) {
+        window.location.href = "login.php";
+    }
+})();
