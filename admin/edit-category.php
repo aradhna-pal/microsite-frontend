e86b39ab803ea8ea2d71c6df45cd3d17b16682f9
imdevkumar <sdev70817@gmail.com>
2026-04-24 11:45:35 +0530

    sdfgd

--- admin/edit-category.php
+++ admin/edit-category.php
@@ -6,7 +6,7 @@
         <!-- main-content-wrap -->
         <div class="main-content-wrap">
             <div class="flex items-center flex-wrap justify-between gap20 mb-27">
-                <h3>Category infomation</h3>
+                <h3>Edit Category infomation</h3>
                 <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
                     <li>
                         <a href="index.php">
@@ -25,7 +25,7 @@
                         <i class="icon-chevron-right"></i>
                     </li>
                     <li>
-                        <div class="text-tiny">New category</div>
+                        <div class="text-tiny">edit category</div>
                     </li>
                 </ul>
             </div>
