1a6112d1c1a3e26b067f3f088a06b160cc6e04f7
ankitchauhan1221 <ankitchauhan1221@gmail.com>
2026-05-12 12:59:59 +0530

    hgfdssdfghj

--- header.php
+++ header.php
@@ -77,10 +77,9 @@
                                 <a href="#">Links</a>
                                 <ul>
                                     <li><a href="tel:#"><i class="icon-phone"></i>Call: +0123 456 789</a></li>
-                                    <li><a href="wishlist.html"><i class="icon-heart-o"></i>My Wishlist
-                                            <span>(3)</span></a></li>
-                                    <li><a href="about.html">About Us</a></li>
-                                    <li><a href="contact.html">Contact Us</a></li>
+                                   
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
                                     <li><a href="#signin-modal" data-toggle="modal"><i class="icon-user"></i>Login</a>
                                     </li>
                                 </ul>
